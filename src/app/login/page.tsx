cee53328aee4b4d561cf207b3f69cae375e1b438
Ömer Yalınkılınç <yalinkilicomer2@gmail.com>
2025-09-13 15:02:03 +0300

    init: MiniCRM frontend yapısı ve login tasarımı başlatıldı

---
+++ src/app/login/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <div>
+        <h1 className="flex justify-center bg-[#eadbf6]">Login</h1>
+      </div>
+    </div>
+  );
+};
+
+export default page;
